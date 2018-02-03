2326e5ccd1ba075ba4db5e81eec01a92e9855d7e
Steven <schen36@stuy.edu>
2018-02-02 21:30:53 -0500

    Created first lab directory

---
+++ 01Recursion/Recursion.java
@@ -0,0 +1,37 @@
+public class Recursion {
+
+    public int fact(int n) {
+	if (n < 0) {
+	    throw new IllegalArgumentException("input can't be negative");
+	}
+
+
+	
+	return 0;
+    }
+
+    public int fib(int n) {
+	if (n < 0) {
+	    throw new IllegalArgumentException("input can't be negative");
+	}
+
+
+	
+	return 0;
+    }
+
+    public double sqrt(double n) {
+	if (n < 0) {
+	    throw new IllegalArgumentException("input can't be negative");
+	}
+
+
+	
+	return 0;
+    }
+    
+    public static void main (String args[]) {
+
+    }
+
+}
