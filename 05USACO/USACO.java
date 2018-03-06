a046a29f74b27f375c9845e078d9acdcf1b0c79b
StevenRamen <schen36@stuy.edu>
2018-03-06 14:44:53 -0500

    working on instructions

--- 05USACO/USACO.java
+++ 05USACO/USACO.java
@@ -14,35 +14,42 @@ public class USACO {
 	    File file = new File(filename);
 	    Scanner scan = new Scanner(file);
 	    
-	    int R = scan.nextInt();
-	    int C = scan.nextInt();
-	    int E = scan.nextInt();
-	    int N = scan.nextInt();
+	    int R = scan.nextInt(); // rows
+	    int C = scan.nextInt(); // cols
+	    int E = scan.nextInt(); // elevation
+	    int N = scan.nextInt(); // # of instructions
+
+	    // sets up board
 	    int[][] lake = new int[R][C];
-	    
 	    for (int r = 0; r < R; r ++) {
 		for (int c = 0; c < C; c ++) {
 		    lake[r][c] = scan.nextInt();
 		}
 	    }
-
+	    /*
 	    int R_s = scan.nextInt();
 	    int C_s = scan.nextInt();
 	    int D_s = scan.nextInt();
-	    /*
-	    System.out.println(R);
-	    System.out.println(C);
-	    System.out.println(E);
-	    System.out.println(N);
-	    System.out.println(R_s);
-	    System.out.println(C_s);
-	    System.out.println(D_s);
-	    System.out.println(Arrays.deepToString(lake));
 	    */
+	    int[][] instructions = new int[N][3];
+	    for (int i = 0; i < N; i ++) {
+		for (int x = 0; x < 3; x ++) {
+		    instructions[i][x] = scan.nextInt();
+		}
+	    }
+
+	    for (int i = 0; i < instructions.length; i ++) {
+		
+		
+		
+		
+	    }
 	    
 	} catch (FileNotFoundException e) {
 	    System.out.println("File Not Found");
 	}
+
+	
 
 	
 	return 0;
