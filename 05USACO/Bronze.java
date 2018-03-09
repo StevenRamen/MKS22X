9899666fe908420bd929fbd3905795f283bf4aca
StevenRamen <schen36@stuy.edu>
2018-03-09 14:44:47 -0500

    working on it

--- 05USACO/Bronze.java
+++ 05USACO/Bronze.java
@@ -31,9 +31,8 @@ public class Bronze {
 	    for (int i = 0; i < N; i ++) {
 		R_s = scan.nextInt() - 1;
 		C_s = scan.nextInt() - 1;
-		D_s = scan.nextInt() - 1;
+		D_s = scan.nextInt();
 		stomp();
-		System.out.println(toString(lake));
 	    }
 	    
 
@@ -45,40 +44,39 @@ public class Bronze {
     }
 
     private static void stomp() {
-	int max = 0;
-	for (int i = 0; i < D_s; i ++) {
-	    
-	    max = lake[R_s][C_s];
-
-	    for (int r = R_s; r < R_s + 3; r ++) {
-		for (int c = C_s; c < C_s + 3; c ++) {
-		    if (lake[r][c] >= max) {
-			max = lake[r][c];
-		    }
+	int max = lake[R_s][C_s];
+	
+	for (int r = R_s; r < R_s + 3; r ++) {
+	    for (int c = C_s; c < C_s + 3; c ++) {
+		if (lake[r][c] >= max) {
+		    max = lake[r][c];
 		}
 	    }
-
-	    int temp = max - D_s;
-	    
-	    for (int r = R_s; r < R_s + 3; r ++) {
-		for (int c = R_s; c < C_s + 3; c ++) {
-		    if (lake[r][c] > temp) {
-		        lake[r][c] = temp;
-		    }
-		}
-	    }
-	    
 	}
+	System.out.println(toString(lake));
+	int temp = max - D_s;
+	System.out.println(temp);
+	for (int r = R_s; r < R_s + 3; r ++) {
+	    for (int c = R_s; c < C_s + 3; c ++) {
+		if (lake[r][c] > temp) {
+		    lake[r][c] = temp;
+		}
+	    }
+	}
+	
     }
 
     public static int solve() {
+	int[][] bleh = new int[R][C];
 	for (int r = 0; r < R; r ++) {
 	    for (int c = 0; c < C; c ++) {
 		if (E - lake[r][c] > 0) {
 		    ans += E - lake[r][c];
+		    bleh[r][c] = E - lake[r][c];
 		}
 	    }
 	}
+	System.out.println(toString(bleh));
 	return ans * 72 * 72;
     }
 
