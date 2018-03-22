46f4eb1a7e90145240617b6d9140ffdf7e2d4b75
Steven <schen36@stuy.edu>
2018-03-21 21:28:34 -0400

    finished merge - sometimes overflow?

--- 07Merge/Merge.java
+++ 07Merge/Merge.java
@@ -33,10 +33,27 @@ public class Merge {
 	return output;
     }
     
-    public static void merge(int[] data, int[] temp, int lo, int hi) {
-	int mid = (lo + hi) / 2;
-
-	for (i
+    public static void merge(int[] data, int[] temp, int lo, int mid, int hi) {
+	int index1 = lo;
+	int index2 = mid + 1;
+	int outputIndex = lo;
+	
+	while (index1 <= mid || index2 <= hi) {
+	    if (index1 > mid) {
+		data[outputIndex] = temp[index2];
+		index2 ++;
+	    } else if (index2 > hi) {
+		data[outputIndex] = temp[index1];
+		index1 ++;
+	    } else if (temp[index1] <= temp[index2]) {
+		data[outputIndex] = temp[index1];
+		index1 ++;
+	    } else {
+		data[outputIndex] = temp[index2];
+		index2 ++;
+	    }
+	    outputIndex ++;
+	}
     }
     
     public static void mergesort(int[] data) {
@@ -46,18 +63,27 @@ public class Merge {
     }
 
     public static void msort(int[] data, int[] temp, int lo, int hi) {
-	int mid = (lo + hi) / 2;
-
-	msort(temp, data, lo, mid);
-	msort(temp, data, mid + 1, hi);
-
-	merge(data, temp, lo, hi);
+	if (lo < hi) {
+	    for (int i = lo; i <= hi; i ++) {
+		temp[i] = data[i];
+	    }
+	    int mid = (lo + hi) / 2;
+	    
+	    msort(temp, data, lo, mid);
+	    msort(temp, data, mid + 1, hi);
+	    
+	    merge(data, temp, lo, mid, hi);
+	}
     }
     
     public static void main(String[] args) {
-	int[] data1 = {1, 56, 85, 3, 54, 234, 321};
-	int[] data2 = {2, 4, 6, 8, 82, 87, 97, 101, 234, 322};
-	System.out.println(Arrays.toString(merge(data1, data2)));
+	//int[] data1 = {1, 56, 85, 3, 54, 234, 321};
+	//int[] data2 = {2, 4, 6, 8, 82, 87, 97, 101, 234, 322};
+	//System.out.println(Arrays.toString(merge(data1, data2)));
+	int[] data = {0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2};
+	int[] data1 = {1, 5, 7, 2, 76, 213, 876, 32, 2, 76, 54, 303, 4534, 2435, 5453};
+	Merge.mergesort(data1);
+	System.out.println(Arrays.toString(data1));
     }
     
 }
