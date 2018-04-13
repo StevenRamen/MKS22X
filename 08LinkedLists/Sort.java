0d23336702da0a87ac5dd7a730ed5e4434890e6a
StevenRamen <schen36@stuy.edu>
2018-04-13 14:46:03 -0400

    basic stuff and helpful comments for later on

--- 08LinkedLists/Sort.java
+++ 08LinkedLists/Sort.java
@@ -1,7 +1,8 @@
 public class Sort{
     
     public static void radixsort(MyLinkedListImproved<Integer> data) {
+	@Suppresswarnings ("unchecked") MyLinkedListImproved <Integer> [] data = new MyLinkedListImproved[10];
 	
     }
-    
+    //log gets # of digits
 }   
