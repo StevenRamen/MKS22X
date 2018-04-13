7e361574efbe163bb07247af1dfee28860a9eedb
StevenRamen <schen36@stuy.edu>
2018-04-13 14:32:01 -0400

    added extend

--- 08LinkedLists/MyLinkedListImproved.java
+++ 08LinkedLists/MyLinkedListImproved.java
@@ -228,5 +228,13 @@ public class MyLinkedListImproved<T extends Comparable<T>> implements Iterable<T
 	}
 	return node.getValue();
     }
+
     
+    public void extend(MyLinkedListImproved<T> other){
+        //in O(1) runtime, move the elements from other onto the end of this
+        //The size of other is reduced to 0
+        //The size of this is now the combined sizes of both original lists
+
+	
+    }
 }
