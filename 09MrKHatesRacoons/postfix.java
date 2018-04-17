7e4e4e0060865f0f04fc61a1f020e04076b78b92
StevenRamen <schen36@stuy.edu>
2018-04-17 14:49:44 -0400

    postfix

---
+++ 09MrKHatesRacoons/postfix.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class postfix {
+
+    public static double String[] eval(String input) {
+	String[] splited = input.split(" ");
+
+	for (int i = 0; i < splited.length; i ++) {
+	    
+	}
+    }
+
+    public static void main(String[] args) {
+	System.out.println(Arrays.toString(eval("123 534 +")));
+    }
+}
