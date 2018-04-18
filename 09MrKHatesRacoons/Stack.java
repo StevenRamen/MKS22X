77ba4f614cd0bf97e1bb69d34ce2310de5d51111
Steven <schen36@stuy.edu>
2018-04-17 23:34:33 -0400

    fixed up some T vs. double stuff

--- 09MrKHatesRacoons/Stack.java
+++ 09MrKHatesRacoons/Stack.java
@@ -2,24 +2,24 @@ import java.util.*;
 
 public class Stack {
 
-    private ArrayList<double> data;
+    private ArrayList<Double> data;
     
     public Stack() {
-	data = new ArrayList<double>();
+	data = new ArrayList<Double>();
     }
 
-    public void push(T value) {
+    public void push(double value) {
 	data.add(value);
     }
 
-    public T pop() {
+    public double pop() {
 	if (data.size() == 0) {
 	    throw new NoSuchElementException();
 	}
 	return data.remove(data.size() - 1);
     }
 
-    public T peek() {
-	
+    public double peek() {
+	return 0;
     }
 }
