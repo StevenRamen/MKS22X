364547e48805bcd08dd9cdea8ad52418168ca460
Steven <schen36@stuy.edu>
2018-04-29 17:33:18 -0400

    adding works

--- 12BBQ/MyHeap.java
+++ 12BBQ/MyHeap.java
@@ -1,21 +1,22 @@
 // n - 1 / 2 - parent
 // 2n 2n + 1 - children
+import java.util.*;
 
-public class MyHeap <t extends Comparable<T>> {
+public class MyHeap <T extends Comparable<T>> {
 
     private T[] data;
     private int size;
     private boolean max;
 
-    @SupressWarnings("unchecked")
+    @SuppressWarnings("unchecked")
     public MyHeap() {
 	data = (T[]) new Comparable[10];
 	size = 0;
 	max = true;
     }
 
-    @SupressWarnings("unchecked")
-    public myHeap(boolean E) {
+    @SuppressWarnings("unchecked")
+    public MyHeap(boolean E) {
 	data = (T[]) new Comparable[10];
 	size = 0;
 	max = E;
@@ -26,16 +27,23 @@ public class MyHeap <t extends Comparable<T>> {
 	    resize();
 	}
 	data[size()] = s;
-	addHelper(s, size());
+	pushUp(size());
 	size ++;
     }
 
-    public void addHelper(T s, int x) {
-	if (s 
+    public void pushUp(int n) {
+        int parent = (n - 1) / 2;
+	if (n == 0){
+	    return;
+	}
+	if (max && data[parent].compareTo(data[n]) < 0 || !max && data[parent].compareTo(data[n]) > 0){
+	    swap(parent, n);
+	    pushUp(parent);
+	}
     }
-
+    
     public String remove() {
-
+	return "";
     }
 
     public T peek() {
@@ -59,6 +67,44 @@ public class MyHeap <t extends Comparable<T>> {
 	}
 	data = temp;
     }
+
+    public String toString() {
+	String ans = "(";
+	for (int i = 0; i < data.length; i ++) {
+	    if (data[i] != null) {
+		ans += data[i] + ", ";
+	    }
+	}
+	return ans + ")";
+    }
     
-    
+    public static void main(String[] args) {
+	MyHeap<Integer> a = new MyHeap<>();
+	/*Integer[] data = new Integer[10];
+	for (int i = 0; i < data.length; i ++) {
+	    data[i] = i;
+	}*/
+	System.out.println(a.size());
+	a.add(0);
+	System.out.println(a.size());
+        System.out.println(a.toString());
+	a.add(1);
+	System.out.println(a.size());
+        System.out.println(a.toString());
+	a.add(3);
+	System.out.println(a.size());
+        System.out.println(a.toString());
+	a.add(4);
+	System.out.println(a.size());
+        System.out.println(a.toString());
+	a.add(2);
+	System.out.println(a.size());
+        System.out.println(a.toString());
+	a.add(5);
+	System.out.println(a.size());
+        System.out.println(a.toString());
+	a.add(11);
+	System.out.println(a.size());
+        System.out.println(a.toString());
+    }
 }
