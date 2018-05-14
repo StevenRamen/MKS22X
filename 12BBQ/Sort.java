81aedd50a90278bd11999c6d60eaaeb7032b1259
Steven <schen36@stuy.edu>
2018-05-14 00:20:39 -0400

    tried to fix sort - messed up even more whoops

--- 12BBQ/Sort.java
+++ 12BBQ/Sort.java
@@ -4,26 +4,19 @@ public class Sort {
     
     public static void heapSort(Integer[] data) {
 	MyHeap<Integer> a = new MyHeap<>();
-	/*
-	a.Heap(data);
-
-	for (int i = data.length - 1; i >= 0; i --) {
-	    data[i] = a.remove(i);
-	}
-	*/
-	for (int i = 0; i < data.length; i ++) {
-	    a.add(data[i]);
-	}
-
-	while (a.size() > 0) {
-	    data[a.size() - 1] = a.remove();
+	int i = data.length - 1;
+        a.toHeap(data);
+	
+	while(i >= 0) {
+	    data[i] = a.remove(i);;
+	    i --;
 	}
 	
     }
 
     public static void main(String[] args){
 	Integer[] a ={1,54,23,3,53,23,455,234,31};
-	heapSort(a);
+	Sort.heapSort(a);
 	System.out.println(Arrays.toString(a));
     }
 }
